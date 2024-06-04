29df78fade5d437203b04253f8715ca0d63e3c59
Misha Cherpak <cherpak.m98@icloud.com>
2024-06-04 22:47:11 +0300

    Fix annotation of parameters

--- app/main.py
+++ app/main.py
@@ -26,14 +26,14 @@ class CarWashStation:
 
         return round(profit, 1)
 
-    def calculate_washing_price(self, car: Any) -> float:
+    def calculate_washing_price(self, car: Car) -> float:
         result = round(car.comfort_class
                        * (self.clean_power - car.clean_mark)
                        * (self.average_rating
                           / self.distance_from_city_center), 1)
         return result
 
-    def wash_single_car(self, car: Any) -> None:
+    def wash_single_car(self, car: Car) -> None:
         car.clean_mark = self.clean_power
 
     def rate_service(self, evaluation: int) -> None:
